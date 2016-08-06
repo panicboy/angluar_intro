7ef6a45b1e82000dcdba4857e66af05d51734997
panicboy <asasaki@gmail.com>
2016-08-06 11:23:00 -1000

    controller

---
+++ public/js/myApp.js
@@ -0,0 +1,16 @@
+angular.module('myApp', []);
+
+var myApp = angular.module('myApp');
+
+myApp
+  .config(function() {
+    // config
+  })
+  .run(function() {
+    // initialize
+  });
+
+  myApp.controller('MyController', [ '$scope', function($scope) {
+      // controler body
+      $scope.myFirstName = 'Andrew';
+    }]);
