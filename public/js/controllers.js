23893d345879df9f0106a96a4892f7fc063bfd58
panicboy <asasaki@gmail.com>
2016-08-06 11:38:55 -1000

    external controller

---
+++ public/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('myApp')
+
+.controller('MyController', [ '$scope', function($scope) {
+  // controler body
+  $scope.myFirstName = 'Andrew';
+  $scope.myModel='Ready Player One';
+}]);
